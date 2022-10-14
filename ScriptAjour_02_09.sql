653241fe40f756d0689f0b3a53825d58d76ccc6d
Yves Theo <theobathresearch@gmail.com>
2022-10-14 06:44:37 +0200

    Scripts SQL

---
+++ ScriptAjour_02_09.sql
@@ -0,0 +1,12 @@
+﻿
+--Sur la BD SSM_Stock
+
+CREATE TABLE [dbo].[LigneRequisitionAutre] (
+    [idrequisition] INT      NOT NULL,
+    [date_jour]     DATE     NOT NULL,
+    [idproduit]       SMALLINT      NOT NULL,
+    [qterequise]    SMALLINT NOT NULL,
+    [qteaccordee]   SMALLINT NOT NULL,
+    PRIMARY KEY CLUSTERED ([idrequisition] ASC),
+    FOREIGN KEY ([idproduit]) REFERENCES [dbo].[ProduitAutreStock] ([idproduit])
+);
